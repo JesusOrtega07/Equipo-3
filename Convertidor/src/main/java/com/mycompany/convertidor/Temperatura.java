908da17da61a5abfef86c738d4da10e2e09682ac
Nao López <naomi.lopez52@unach.mx>
2025-06-09 15:03:41 -0600

    modulo2

---
+++ Convertidor/src/main/java/com/mycompany/convertidor/Temperatura.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.convertidor;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author naomilopezdelacruz
+ */
+public class Temperatura {
+    
+    Menu obj = new Menu();
+    
+    Scanner scanner = new Scanner (System.in);
+    
+    public void convertirCelsiusFar(){
+        System.out.println("Escrible los celsius a convertir:");
+        obj.celsius = scanner.nextInt();
+        
+        obj.kilometros = (obj.metros/1000);
+        
+        System.out.println("Estos son los kilometros" + obj.kilometros);
+        
+    }
+      
+    
+    public void convertirCentimetros(){
+        System.out.println("Escrible los centimetros a convertir:");
+        obj.metros = scanner.nextInt();
+        
+        obj.metros = (obj.centimetros/100);
+        
+        System.out.println("Estos son los kilometros" + obj.metros);
+        
+    }
+            
+}
