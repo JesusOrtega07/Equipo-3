a3e6abe9d5c22f3a45bb8d38be058a5d450fcd08
Carlos <carloszavalabarrios867@gmail.com>
2025-06-09 14:45:45 -0600

    primer

---
+++ Convertidor/src/main/java/com/mycompany/convertidor/Convertidor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.convertidor;
+
+/**
+ *
+ * @author carlo
+ */
+public class Convertidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Convertidor/src/main/java/com/mycompany/convertidor/Menu.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.convertidor;
+
+/**
+ *
+ * @author carlo
+ */
+public class Menu {
+
+    public double celsius;
+    public double fahrenheit;
+    public double kelvin;
+    
+    public double metros;
+    public double kilometros;
+    public double centimetros;
+    
+    
+    public int menu(int opcion) {
+        System.out.println("-----MENU-----");
+        System.out.println("1._ longitudes");
+        System.out.println("2._ temperaturas");
+        return opcion;
+    }
+    
+    public int submenulongitudes(int opcion) {
+        System.out.println("-----MENU-----");
+        System.out.println("1._ celsius a fahrenheit");
+        System.out.println("2._ celcius a kelvin");
+        System.out.println("2._ fahrenheit a celcius");
+        System.out.println("2._ kelvin a celcius");
+        return opcion;
+    }
+    
+    public int submenutemperaturas(int opcion) {
+        System.out.println("-----MENU-----");
+        System.out.println("1._ metros a kilometros");
+        System.out.println("2._ centimetros a metros");
+        return opcion;
+    }
+    
+    
+}
